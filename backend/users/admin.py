d92b427dcc2f420611f4b237459065b71947fb9a
Robbie Murray <rob@mur.org.uk>
2025-10-03 23:42:24 +0100

    Revert user admin subclass

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .models import User
 
 
-@admin.register(User)
-class UserAdminExtended(UserAdmin):
-	pass
+admin.site.register(User, UserAdmin)
