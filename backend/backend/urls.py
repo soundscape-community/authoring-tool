3807e2795e96a603f91723f9b1ffa1b7a3da644f
Robbie Murray <rob@mur.org.uk>
2025-11-03 11:11:11 +0000

    Add django-health-check for application health monitoring

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
 
+    # Health check endpoint
+    path('ht/', include('health_check.urls')),
+
     # Serve API app
     path('api/', include('api.urls')),
 
