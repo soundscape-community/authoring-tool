8c8ca684147b4b0f2d25cf755e93ac5b94cdd6d1
Robbie Murray <rob@mur.org.uk>
2026-02-08 23:20:02 +0000

    Add folder delete modal test

---
+++ frontend/src/components/Modals/__tests__/FolderDeleteModal.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import FolderDeleteModal from '../FolderDeleteModal';
+
+describe('FolderDeleteModal', () => {
+  it('passes moveActivities state to onDelete', async () => {
+    const user = userEvent.setup();
+    const onDelete = vi.fn(() => Promise.resolve());
+
+    render(
+      <FolderDeleteModal
+        show={true}
+        folder={{ id: 'folder-1', name: 'Old Folder' }}
+        onCancel={() => {}}
+        onDelete={onDelete}
+      />,
+    );
+
+    const checkbox = screen.getByLabelText('Move activities to Unfoldered');
+    expect(checkbox).toBeChecked();
+
+    await act(async () => {
+      await user.click(checkbox);
+    });
+
+    await act(async () => {
+      await user.click(screen.getByRole('button', { name: 'Delete' }));
+    });
+
+    expect(onDelete).toHaveBeenCalledWith({ moveActivities: false });
+  });
+});
